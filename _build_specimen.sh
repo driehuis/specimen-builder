d3382335523ed1a3150129339b69e63c56f37ab4
Bert Driehuis <driehuis@playbeing.org>
2025-01-23 13:26:49 +0100

    Updates for safety. Take font from temporary build environment.

--- _build_specimen.sh
+++ _build_specimen.sh
@@ -7,11 +7,15 @@ docker build --progress=plain . -t specimen-builder
 
 rm -f src/fonts/*.woff2
 
-cp ../dinish/fonts/woff2/variable/*.woff2 src/fonts/
+#cp ../dinish/fonts/woff2/variable/*.woff2 src/fonts/
+cp /dev/shm/dinish-build/fonts/woff2/variable/*.woff2 src/fonts/
+#cp /dev/shm/dinish-build/variable_ttf/*.woff2 src/fonts/
+
+[ -d out ] || mkdir out
 
 docker run --rm -v `pwd`/src:/app/src -v `pwd`/out:/out specimen-builder
 
 (
-	cd out/_site
+	cd out/_site &&
 	python3 ../../standalone_html.py index.html /tmp/compact.html
 )
